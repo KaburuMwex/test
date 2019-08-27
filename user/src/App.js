538ed594fe05224d5840ef707674e41a2c26a6db
unknown <matheejunior@gmail.com>
2019-08-27 18:41:27 +0300

    first commit

---
+++ user/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+       <h1>HELLO WORLD</h1>
+       <p>Test for netlify</p>
+    </div>
+  );
+}
+
+export default App;
